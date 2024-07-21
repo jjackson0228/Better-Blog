46305ae6e90ea69449665f7c1032f37fb2188f43
James Jackson <jjackson0228@gmail.com>
2024-07-21 00:22:02 -0400

    got light and dark mode working and the icon for togglemode

---
+++ assets/js/form.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById("toggle-mode");
+const body = document.body;
+
+toggleButton.addEventListener("click", function () {
+  body.classList.toggle("dark-mode");
+});
