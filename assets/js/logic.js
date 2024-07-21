7a4f52acd986e6ae76ff788dea65d569694d7b9d
James Jackson <jjackson0228@gmail.com>
2024-07-21 01:15:17 -0400

    added icon to back button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,11 @@
 const toggleButton = document.getElementById("toggle-mode");
 const body = document.body;
+const backBtn = document.getElementById("back-button");
 
 toggleButton.addEventListener("click", function () {
   body.classList.toggle("dark-mode");
+});
+
+backBtn.addEventListener("click", function () {
+  window.history.back();
 });
