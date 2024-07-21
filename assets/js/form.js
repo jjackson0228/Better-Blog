54301d8c43f86fa00829ad0f671577d70c88355c
James Jackson <jjackson0228@gmail.com>
2024-07-21 00:24:01 -0400

    fixed javascript to logic.js

--- assets/js/form.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementById("toggle-mode");
-const body = document.body;
-
-toggleButton.addEventListener("click", function () {
-  body.classList.toggle("dark-mode");
-});
